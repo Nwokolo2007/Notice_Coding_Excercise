3b951ddb4b0c7eb89648889e58c6d6f99da4342d
Nwokolo_chibuzo <nwokolo_chibuzo@outlook.com>
2022-04-08 16:09:19 +0200

    completed the front end part of the assignment

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+
+const titles = ["Notice Coding Excercise","Another title"];
+
+function changeTitleHandler()
+{
+    
+   let currentTitle =  document.getElementById("title");
+    let currentIndex = 0;
+    if(currentTitle == null)
+    {
+        alert("The title tag does not exist on the DOM");
+    }
+   
+    let title = currentTitle.innerHTML;
+    currentIndex = titles.indexOf(title);
+    currentIndex = ((currentIndex +1) >= titles.length) ? 0: currentIndex+1;
+
+    currentTitle.innerHTML = titles[currentIndex];
+
+    return;
+}
+
+function setUserName()
+{
+
+    let userName = document.getElementById("username").value;
+    if(userName === "")
+    {
+        alert("please enter a user name");
+        return false;
+    }
+    localStorage.setItem("userName",userName);
+    let display =  document.getElementById("display");
+    display.innerHTML = userName;
+    return false;
+}
+
+ window.onload =  function getUserName()
+{
+    let userName =  localStorage.getItem("userName");
+    let display =  document.getElementById("display");
+    display.innerHTML = userName;
+    return;
+}
+
