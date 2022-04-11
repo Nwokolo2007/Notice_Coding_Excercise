23b0b79a8613fb6b8a9d03c0d5050c9e741e47a3
Nwokolo_chibuzo <nwokolo_chibuzo@outlook.com>
2022-04-11 10:55:36 +0200

    separted the front and backend git repos

--- server/controllers/noticeController.js
+++
@@ -1,8 +0,0 @@
-
-
-exports.getCurrentTime = (req, res, next) => {
-
-    let time = new Date().toString();
-    res.status(200).json({currentTime:time});
-
-};
--- server/routes/noticeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express');
-const noticeController = require('../controllers/noticeController');
-
-const router = express.Router();
-
-router.get('/time', noticeController.getCurrentTime);
-
-module.exports = router;
